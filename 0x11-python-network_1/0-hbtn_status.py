c3fbac3d3c42f65d5f3b264ecc41f39cea158135
EREEK71 <ericelesho@gmail.com>
2022-12-26 00:58:29 -0800

    pp

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-'''Script that fetches https://alx-intranet.htbn.io/status'''
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
 
 
 if __name__ == '__main__':
     import urllib.request
 
-text = '''Body response:\n\t- type: \
-{}\n\t- content: {}\n\t- utf8 content: {}'''
-    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        result = response.read()
-        print(text.format(type(result), result, str(result)[2:-1]))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
